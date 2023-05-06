70d4fcac9b3c20413e750ab8a89d154a94d04d41
stefandzeladinovic <stefan.dzeladinovic@enjoying.rs>
2023-05-06 14:44:28 +0200

    First commit

---
+++ FirstJavaApp/src/MainAppPackage/MainAppClass.java
@@ -0,0 +1,10 @@
+package MainAppPackage;
+
+public class MainAppClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Stefan's app Git is funny");
+	}
+
+}
---
+++ FirstJavaApp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author stdz2709
+ *
+ */
+module FirstJavaApp {
+}
